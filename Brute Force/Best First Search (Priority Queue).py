4fb6848cc11442dec3e8f30895ce3712efd38b52
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2022-12-25 22:11:32 +0800

    Update Best First Search (Priority Queue).py

--- Brute Force/Best First Search (Priority Queue).py
+++ Brute Force/Best First Search (Priority Queue).py
@@ -1,108 +1,186 @@
-import copy
+from copy import deepcopy
 import time
 
+# matrix direction
+MOVES = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
+# matrix target
+FINAL = [[1, 2, 3], [4, 5, 0], [6, 7, 8]]
 
-def printNode(node):
 
-    print(node[0], node[1], node[2])
-    print(node[3], node[4], node[5])
-    print(node[6], node[7], node[8])
+class Node:
 
-    global nodeNumber
-    print('Node:', nodeNumber)
-    print('Depth:', len(node[9:]))
-    print('Moves:', node[9:])
+    def __init__(self, current_node, previous_node, x, y, moves):
+        self.current_node = current_node
+        self.previous_node = previous_node
+        self.x = x
+        self.y = y
+        self.moves = moves
+
+    def f(self):
+        return self.x + self.y
+
+
+def get_position(current_state, element):
+
+    for row in range(len(current_state)):
+        if element in current_state[row]:
+            return (row, current_state[row].index(element))
+
+
+def getAdjNode(node):
+
+    listNode = []
+    emptyPos = get_position(node.current_node, 0)
+
+    for moves in MOVES.keys():
+
+        newPos = (emptyPos[0] + MOVES[moves][0], emptyPos[1] + MOVES[moves][1])
+
+        if 0 <= newPos[0] < len(node.current_node) and 0 <= newPos[1] < len(node.current_node[0]):
+            newState = deepcopy(node.current_node)
+            newState[emptyPos[0]][emptyPos[1]] = node.current_node[newPos[0]][newPos[1]]
+            newState[newPos[0]][newPos[1]] = 0
+
+            listNode.append(Node(newState, node.current_node, node.x + 1, calculateCost(newState), moves))
+
+    return listNode
+
+
+def calculateCost(current_state):
+
+    cost = 0
+
+    for row in range(len(current_state)):
+        for col in range(len(current_state[0])):
+            pos = get_position(FINAL, current_state[row][col])
+            cost += abs(row - pos[0]) + abs(col - pos[1])
+
+    return cost
+
+
+def getBestNode(openSet):
+
+    firstIteration = True
+
+    for node in openSet.values():
+
+        if firstIteration or node.f() < bestF:
+            firstIteration = False
+            bestNode = node
+            bestF = bestNode.f()
+
+    return bestNode
+
+
+def searchPath(set):
+
+    node = set[str(FINAL)]
+    branch = list()
+
+    while node.moves:
+
+        branch.append({
+            'moves': node.moves,
+            'node': node.current_node
+        })
+
+        node = set[str(node.previous_node)]
+
+    branch.append({
+        'moves': '',
+        'node': node.current_node
+    })
+
+    branch.reverse()
+
+    return branch
+
+
+def printResult(resultArray):
+
     print('------')
-    nodeNumber += 1
+
+    j = 0
+
+    for x in range(len(resultArray)):
+
+        for i in resultArray[x]:
+
+            if (i != 0):
+                print(i, end=" ")
+            else:
+                print(end="0 ")
+
+            j += 1
+
+            if (j >= 3):
+                print()
+                j = 0
+
+    global depth
+    depth += 1
 
 
-def checkFinal(node):
+def main(puzzle):
 
-    if node[:9] == finalNode:
-        printNode(node)
-        return True
+    open_set = {str(puzzle): Node(puzzle, puzzle, 0, calculateCost(puzzle), "")}
+    closed_set = {}
 
-    if node[:9] not in visitedList:
-        printNode(node)
-        nodeList.append(node)
-        visitedList.append(node[:9])
+    while True:
 
-    return False
+        test_node = getBestNode(open_set)
+        closed_set[str(test_node.current_node)] = test_node
 
+        if test_node.current_node == FINAL:
+            return searchPath(closed_set)
 
-def calculateHeuristic(node):
+        adj_node = getAdjNode(test_node)
 
-    distance = 0
+        for node in adj_node:
 
-    for current, target in enumerate(node):
-        currentRow = int(current / 3)
-        currentColumn = current % 3
-        targetRow = int(target / 3)
-        targetColumn = target % 3
-        distance += abs(currentRow - targetRow) + abs(currentColumn - targetColumn)
+            if str(node.current_node) in closed_set.keys() or str(node.current_node) in open_set.keys() and open_set[str(node.current_node)].f() < node.f():
+                continue
 
-    return distance
+            open_set[str(node.current_node)] = node
+
+        del open_set[str(test_node.current_node)]
 
 
 if __name__ == '__main__':
 
-    startNode = [4, 1, 3, 6, 2, 5, 7, 8, 0]
-    finalNode = [1, 2, 3, 4, 5, 0, 6, 7, 8]
-
-    found = False
-
-    nodeNumber = 0
-    visitedList = []
-    nodeList = []
-    nodeList.append(startNode)
-    visitedList.append(startNode)
-    printNode(startNode)
+    # this is start matrix
     t0 = time.time()
+    matrix = main([
+        [4, 1, 3],
+        [6, 2, 5],
+        [7, 8, 0]
+    ])
 
-    while (not found and not len(nodeList) == 0):
+    depth = 0
 
-        fList = []
+    for b in matrix:
 
-        for node in nodeList:
-            h = calculateHeuristic(node[:9])
-            g = len(node[9:])
-            f = g + h
-            fList.append(f)
+        if b['moves'] != '':
 
-        currentNode = nodeList.pop(fList.index(min(fList)))
-        blankIndex = currentNode.index(0)
+            letter = ''
 
-        if blankIndex != 0 and blankIndex != 1 and blankIndex != 2:
+            printResult(b['node'])
 
-            upNode = copy.deepcopy(currentNode)
-            upNode[blankIndex] = upNode[blankIndex - 3]
-            upNode[blankIndex - 3] = 0
-            upNode.append('up')
-            found = checkFinal(upNode)
+            if b['moves'] == 'U':
+                letter = 'up'
+            elif b['moves'] == 'R': \
+                    letter = "right"
+            elif b['moves'] == 'L':
+                letter = 'left'
+            elif b['moves'] == 'D':
+                letter = 'down'
 
-        if blankIndex != 0 and blankIndex != 3 and blankIndex != 6 and found == False:
+            print('Depth:', depth)
+            print('Move:', letter)
 
-            leftNode = copy.deepcopy(currentNode)
-            leftNode[blankIndex] = leftNode[blankIndex - 1]
-            leftNode[blankIndex - 1] = 0
-            leftNode.append('left')
-            found = checkFinal(leftNode)
-
-        if blankIndex != 6 and blankIndex != 7 and blankIndex != 8 and found == False:
-
-            downNode = copy.deepcopy(currentNode)
-            downNode[blankIndex] = downNode[blankIndex + 3]
-            downNode[blankIndex + 3] = 0
-            downNode.append('down')
-            found = checkFinal(downNode)
-
-        if blankIndex != 2 and blankIndex != 5 and blankIndex != 8 and found == False:
-
-            rightNode = copy.deepcopy(currentNode)
-            rightNode[blankIndex] = rightNode[blankIndex + 1]
-            rightNode[blankIndex + 1] = 0
-            rightNode.append('right')
-            found = checkFinal(rightNode)
+    print()
+    print('------')
+    print('Total steps: ', len(matrix) - 1)
 
     t1 = time.time()
     print('Time:', t1 - t0)
